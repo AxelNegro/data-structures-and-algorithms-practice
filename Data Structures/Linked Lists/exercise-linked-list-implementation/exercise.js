15e2caf5b771ac3fc9bb0860ddbd6512d5e56a0c
unknown <axelnperez@hotmail.com>
2022-12-05 22:32:48 +0100

    Added append to my LinkedList implementation

---
+++ Data Structures/Linked Lists/exercise-linked-list-implementation/exercise.js
@@ -0,0 +1,39 @@
+// Create the below linked list:
+// myLinkedList = {
+//   head: {
+//     value: 10
+//     next: {
+//       value: 5
+//       next: {
+//         value: 16
+//         next: null
+//       }
+//     }
+//   }
+// };
+
+class LinkedList {
+  constructor(value) {
+    this.head = {
+      value: value,
+      next: null
+    };
+    this.tail = this.head;
+    this.length = 1;
+  }
+  append(value) {
+    const lastNode = {
+      value: value,
+      next: null
+    };
+    this.tail.next = lastNode;
+    this.tail = lastNode;
+    this.length++;
+  }
+}
+
+let myLinkedList = new LinkedList(10);
+myLinkedList.append(5);
+myLinkedList.append(16);
+myLinkedList.list();
+
