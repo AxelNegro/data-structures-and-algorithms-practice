fa3097708f7095849f0dd0fea7e0b6f6b0c88ed0
unknown <axelnperez@hotmail.com>
2022-12-29 13:39:39 +0100

    added the solution from the course

--- Algorithms/Sorting/exercise-bubble-sort.js
+++ Algorithms/Sorting/exercise-bubble-sort.js
@@ -38,5 +38,19 @@ function checkSort(array){
   return true; // if the code reaches this part, then it's sorted
 }
 
+function solutionBubbleSort(array){
+const length = array.length;
+  for (let i = 0; i < length; i++) {
+    for (let j = 0; j < length; j++) { 
+      if(array[j] > array[j+1]) {
+        //Swap the numbers
+        let temp = array[j]
+        array[j] = array[j+1];
+        array[j+1] = temp;
+      }
+    }        
+  }
+}
+
 bubbleSort(numbers);
 console.log(numbers);
